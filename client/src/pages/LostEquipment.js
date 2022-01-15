0bce65dd846a98a4627e5e8a85ca9071eca67f24
Matthew Sievers <mattersievers@gmail.com>
2022-01-15 06:40:14 -0600

    homepage loads lost equipment

--- client/src/pages/LostEquipment.js
+++ client/src/pages/LostEquipment.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { useQuery } from '@apollo/client';
+import { GET_EQUIPMENT } from '../utils/queries';
+
 
 const LostEquipment = () => {
+    const { loading, data: userData } = useQuery(GET_EQUIPMENT);
+ 
+    if(loading) {
+        return <h1> LOADING... </h1>
+    }
+
     return(
         <div className="lostEquipmentContent mainContent">
-
+            <h1>
+                Welcome to Lost and Sound!
+            </h1>
+            <h2>
+                Report and recover lost or stolen musical instruments and accessories.
+            </h2>
+            <div className="align-self-center userEquipment">
+                {userData.users.map((user) => {
+                    if(user.hasLost) {
+                        return (
+                            <>
+                                <div className="userName">
+                                    {user.firstName} {user.lastName}
+                                </div>
+        
+                                {user.savedEquipment.map((item) => {
+                                    if (item.lost) {
+                                        return(
+                                            <ul>
+                                                <li>Category: `{item.category}`</li>
+                                                <li>Brand: `{item.brand}`</li>
+                                                <li>Model: `{item.model}`</li>
+                                                <li>Description: `{item.description}`</li>
+                                                <li>Serial Number:  `{item.serialNumber}`</li>
+                                        </ul>
+                                        )
+                                    }
+                                })}
+                            </>
+                        )
+                    }
+                    
+                })}
+            </div>
         </div>
     )
 }
