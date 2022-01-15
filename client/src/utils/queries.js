5617005ccf67a9dff60628dad9ec565b7d7e9ba6
Matthew Sievers <mattersievers@gmail.com>
2022-01-15 01:10:51 -0600

    loginForm and signupForm added

---
+++ client/src/utils/queries.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+{
+    me{
+        firstName
+        lastName
+        email
+        savedEquipment{
+            _id
+            category
+            brand
+            model
+            description
+            serialNumber
+            image
+            location
+            lost
+        }    
+    }
+}`;
+
+export const GET_EQUIPMENT = gql`
+{
+    users {
+        firstName
+        lastName
+        hasLost
+        savedEquipment{
+            _id
+            category
+            brand
+            model
+            description
+            serialNumber
+            image
+            location
+            lost
+        }    
+    }
+}`;
+
+
+export const GET_MY_EQUIPMENT = gql`
+{
+    me {
+        firstName
+        lastName
+        savedEquipment{
+            _id
+            category
+            brand
+            model
+            description
+            serialNumber
+            image
+            location
+            lost
+        }    
+    }
+}`;
+
+export const GET_USERS = gql` 
+{
+    users{
+      _id
+      firstName
+      lastName
+      email
+      savedEquipment{
+        _id
+        category
+        brand
+        serialNumber
+        location
+        model
+      }
+    }
+}`;
+
+export const DONATE = gql`
+    query getDonation($amount: Int!){
+        donate(amount: $amount) {
+            session
+        }
+    }
+`;
